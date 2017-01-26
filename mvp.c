6b478e2357fb7eac73e6fa75a169c7ff97029258
joeylmaalouf <joeylmaalouf@gmail.com>
2017-01-26 12:42:38 -0500

    upgrading mvp (read stdin as fp, parse individual chars)

--- mvp.c
+++ mvp.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+/**/
+int parse () {
+  //
+}
+
+/* execute: forks a new process to run the given command,
+ *          along with any arguments provided
+ * args: array of strings (character arrays)
+ *       representing the command and other arguments 
+ * returns: nothing
+ */
 void execute (char** args) {
   pid_t pid;
   int status;
@@ -23,14 +35,37 @@ void execute (char** args) {
   }
 }
 
-int main(int argc, char* argv[]) {
-  char* args[1];
-  printf("» ");
-  while (scanf("%s", args[0]) != EOF) {
-    printf("%s\n", args[0]);
-    execute(args);
+/* main: reads commands and args from stdin (batch file or
+ *       user input) and executes them in order after parsing
+ * argc: the number of command line arguments
+ * argv: the values of command line arguments
+ * returns: exit code
+ */
+int main (int argc, char* argv[]) {
+  FILE *fp;
+  char c;
+  int interactive;
+  int flag;
+  char* args[256];
+
+  if (argc > 1) {
+    fp = fopen(argv[1], "r");
+    interactive = 0;
+  }
+  else {
+    fp = stdin;
+    interactive = 1;
     printf("» ");
   }
-  puts("\n");
+  do {
+    c = fgetc(fp);
+    flag = parse(c, args);
+    /* handle exit codes from parse to know if it's time to execute a command or just split into next arg */
+    if (interactive && c == '\n') {
+      printf("» ");
+    }
+  } while (c != EOF);
+  printf("\n");
+
   return 0;
 }
