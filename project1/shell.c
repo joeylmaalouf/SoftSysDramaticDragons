c195a1fbe022441f8d5f9369323a9cb920a10375
joeylmaalouf <joeylmaalouf@gmail.com>
2017-02-06 18:36:07 -0500

    moved blank command check from exec to main, added comment functionality

--- project1/shell.c
+++ project1/shell.c
@@ -1,6 +1,10 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /* parse: check an individual character and either
  *        add it to a currently-building command or
@@ -12,20 +16,27 @@
  * returns: flag telling main code whether a command is fully parsed
  */
 int parse (char c, char args[256][256], int* count) {
+  static int in_comment = 0;
   static int in_quote = 0;
   static int in_whitespace = 1;
   static int i_cmd = 0;
   static int i_char = 0;
-  switch(c) {
+  if (in_comment && (c != '\n')) {
+    return 0;
+  }
+  switch (c) {
+    case '#':
+      in_comment = 1;
+      return 0;
     case '"':
       in_quote = !in_quote;
       return 0;
     case EOF:
-      // do extra processing so ctrl-D exits immediately, even if other commands preceded it?
-    case ';':
-      // figure out why ; stopped working
     case '\n':
+      in_comment = 0;
+      in_whitespace = 0;
     case '\0':
+    case ';':
       /* characters separating commands */
       if (!in_quote) {
         if (!in_whitespace) {
@@ -63,8 +74,11 @@ int parse (char c, char args[256][256], int* count) {
  * returns: flag saying whether the string is only whitespace
  */
 int is_blank (char* string) {
-  int i;
-  for (i = 0; i < strlen(string); ++i) {
+  int i, l = strlen(string);
+  if (l < 1) {
+    return 1;
+  }
+  for (i = 0; i < l; ++i) {
     if (!isspace(string[i])) {
       return 0;
     }
@@ -80,20 +94,8 @@ int is_blank (char* string) {
  * returns: nothing
  */
 void execute (char** args) {
-  int i = 0;
-  int blank_status = 1;
   int wait_status;
   pid_t pid;
-
-  /* make sure we have at least one non-empty word */
-  while (args[i] != NULL) {
-    if (!is_blank(args[i++])) {
-      blank_status = 0;
-    }
-  }
-  if (blank_status) {
-    return;
-  }
 
   /* make sure we can fork a new process */
   pid = fork();
@@ -104,7 +106,7 @@ void execute (char** args) {
   else if (pid == 0) {
     /* make sure we can execute the command */
     if (execvp(*args, args) < 0) {
-      fprintf(stderr, "Error: could not execute command\n");
+      fprintf(stderr, "Error: could not execute command \"%s\"\n", args[0]);
       return;
     }
   }
@@ -124,11 +126,11 @@ void execute (char** args) {
 int main (int argc, char* argv[]) {
   FILE *fp;
   char c;
-  int interactive;
+  int interactive, ready;
   char tmp[256][256];
   char** args;
   int count = 0;
-  int i;
+  int i, j;
 
   if (argc > 1) {
     fp = fopen(argv[1], "r");
@@ -141,16 +143,20 @@ int main (int argc, char* argv[]) {
   }
   do {
     c = fgetc(fp);
-    if (parse(c, tmp, &count)) {
-      args = malloc(count * sizeof(char*));
+    ready = parse(c, tmp, &count);
+    if (ready) {
+      args = calloc(count, sizeof(char*));
+      j = -1;
       for (i = 0; i < count; ++i) {
-        args[i] = malloc(256 * sizeof(char));
-        strcpy(args[i], tmp[i]);
+        if(!is_blank(tmp[i])) {
+          args[++j] = calloc(256, sizeof(char));
+          strcpy(args[j], tmp[i]);
+        }
       }
       execute(args);
       count = 0;
     }
-    if (interactive && c == '\n') {
+    if (interactive && (c == '\n')) {
       printf("» ");
     }
   } while (c != EOF);
@@ -160,8 +166,7 @@ int main (int argc, char* argv[]) {
   return 0;
 }
 
+// potential tweaks:
 // args counter var (count)?
 // free args[i]
-// free tmp
-// try not to use tmp at all? currently we have static memory allocation for parse and dynamic for execute
 // increment pointer instead of separate index vars?
